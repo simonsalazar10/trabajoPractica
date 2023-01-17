0fd00587e0faed5fc1087a369b2495e5957e7239
simonsalazar10 <simonluisamigo@gmail.com>
2023-01-17 15:55:51 -0500

    se le aplica un menu, se trabaja plantilla footer

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./components/layouts/Main";
 import Menu from "./components/helpers/Menu";
+/* import Footer from "./components/helpers/Footer"; */
 function App() {
   return (
     <section>
       <Menu />
       <Main />
+      {/* <Footer/> */}
     </section>
   );
 }
--- front/src/components/helpers/Menu.js
+++ front/src/components/helpers/Menu.js
@@ -1,9 +1,21 @@
-import filaUnoImgs from "./FilaUnoImgs";
+import logo1 from "../../imgs/nafnaf.png";
+import logo2 from "../../imgs/rifle.png";
+import logo3 from "../../imgs/esprit.png";
+import logo4 from "../../imgs/chevignon.png";
+import logo5 from "../../imgs/americanino.png";
+import logo6 from "../../imgs/americaneagle.png";
+
 const menu = () => {
     return(
         <section className="header">
-        <img className="chevignon" src="../../../public/chevignon.png" alt="hola" />
-        <h1 className="tituloPrincipal">GRUPO URIBE </h1>   
+        <img className="americanino" src={logo5} alt="hola" />
+        <img className="rifle" src={logo2} alt="hola" />
+        <img className="esprit" src={logo3} alt="hola" />
+        <h1 className="tituloPrincipal">~ GRUPO URIBE ~</h1>
+        <img className="chevignon" src={logo4} alt="hola" /> 
+        <img className="nafnaf" src={logo1} alt="hola" />
+        <img className="americaneagle" src={logo6} alt="hola" />
+
         </section>
         
     )
